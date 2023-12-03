eb519d94a9fc01865cf1973f14087d916f55b316
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-12-04 02:48:04 +0300

    Env files created for dev and prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `./.env/${process.env.NODE_ENV}.env`,
+    }),
+  ],
 })
 export class AppModule {}

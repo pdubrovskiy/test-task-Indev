ce6819ec12be419e530222afde1ef3bab4e98047
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-12-04 05:42:39 +0300

    Auth service implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { Messages } from 'src/constants/messages';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
@@ -8,4 +17,49 @@ export class AuthService {
     private userService: UsersService,
     private jwtService: JwtService,
   ) {}
+
+  public async login(userDto: CreateUserDto): Promise<{ token: string }> {
+    const user = await this.validateUser(userDto);
+    return this.generateToken(user);
+  }
+
+  public async registration(
+    userDto: CreateUserDto,
+  ): Promise<{ token: string }> {
+    const candidate = await this.userService.getUserByEmail(userDto.email);
+    if (candidate) {
+      throw new HttpException(Messages.USER_EXISTS, HttpStatus.BAD_REQUEST);
+    }
+    const hashedPassword = await bcrypt.hash(userDto.password, 5);
+    const user = await this.userService.createUser({
+      ...userDto,
+      password: hashedPassword,
+    });
+    return this.generateToken(user);
+  }
+
+  public async forgetPassword() {}
+
+  public async resetPassword() {}
+
+  private async generateToken(user: User): Promise<{ token: string }> {
+    const payload = { email: user.email, id: user.id };
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+
+  private async validateUser(userDto: CreateUserDto): Promise<User> {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    const passwordEquals = await bcrypt.compare(
+      userDto.password,
+      user.password,
+    );
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({
+      message: Messages.INCORRECT_EMAIL_OR_PASSWORD,
+    });
+  }
 }

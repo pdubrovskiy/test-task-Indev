e9393be1414aaec7edbc716c2437294f4208998c
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-12-04 05:39:06 +0300

    Auth controller implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 
+@ApiTags('Authorization')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  login(@Body() userDto: CreateUserDto): Promise<{ token: string }> {
+    return this.authService.login(userDto);
+  }
+
+  @Post('/registration')
+  registration(@Body() userDto: CreateUserDto): Promise<{ token: string }> {
+    return this.authService.registration(userDto);
+  }
 }

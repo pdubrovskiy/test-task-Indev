f922c7e10a35ab74b7b57f296b69c3328cf3de54
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-12-04 03:38:46 +0300

    Swagger connected

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
 
 async function start() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
 
   const PORT = process.env.PORT || 5000;
 

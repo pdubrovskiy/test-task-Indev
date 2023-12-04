df312b7c451822da6ce64f4c4537a3a0120c3b1e
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-12-04 07:53:42 +0300

    New messages added

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -2,4 +2,10 @@ export enum Messages {
   USER_EXISTS = 'User with that email already exists',
   INCORRECT_EMAIL_OR_PASSWORD = 'Incorrect email or password',
   UNAUTHORIZED = 'User unauthorized',
+  ACTIVATE_YOUR_EMAIL = 'Activation letter was sent to the email',
+  WRONG_LINK = 'Wrong activation link',
+  SUCCESSFUl_ACTIVATION = 'Congratulations, Successful Activation!',
+  SUCCESSFUl_PASSWORD_CHANGE = 'Successful password change',
+  WRONG_EMAIL = 'Wrong email',
+  RESET_LETTER_SENT = 'Letter with password reset was sent to your email',
 }

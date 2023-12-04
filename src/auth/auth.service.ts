97eef3e2c405c002dc6c3beb3991da472e9ac753
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-12-04 04:56:23 +0300

    Auth resource created

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+}
